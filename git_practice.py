2c0fbadee7c65d19ebf37efce0d421366612b985
domtom1126 <domtom1126@gmail.com>
2020-09-10 16:49:42 -0700

    This is the first pull request on new branch(new_pull1)

--- git_practice.py
+++ git_practice.py
@@ -2,4 +2,4 @@
 
 #this is my comment, let's create a simple print statement, character by character and keep committing and pushing, etc.add()
 
-thestring = "This is the first change"
+thestring = "This is gonna be commited on a new branch (new_pull1) now the master branch should just say 'This is the first commit' "
