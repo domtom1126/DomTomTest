446d6f8f8da99e31e7399788ff727d26408f293e
johntaylorpa <johntaylorpa@outlook.com>
2020-09-10 20:08:55 -0400

    added a second string Thursday night

--- git_practice.py
+++ git_practice.py
@@ -3,3 +3,5 @@
 #this is my comment, let's create a simple print statement, character by character and keep committing and pushing, etc.
 
 thestring = "This is gonna be commited on a new branch (new_pull1) now the master branch should just say 'This is the first commit' "
+
+secondstring = "more strings for this code"
