23a854b879ee081c9dea4a4170b2961dc7d3923c
domtom1126 <domtom1126@gmail.com>
2020-09-10 17:19:49 -0700

    I changed it back og stuff

--- git_practice.py
+++ git_practice.py
@@ -1,7 +1,5 @@
 #working on git
 
-#this is my comment, let's create a simple print statement, character by character and keep committing and pushing, etc.
+#this is my comment, let's create a simple print statement, character by character and keep committing and pushing, etc.add()
 
-thestring = "This is gonna be commited on a new branch (new_pull1) now the master branch should just say 'This is the first commit' "
-
-secondstring = "more strings for this code"
+thestring = "This is gonna be commited on a new branch (new_pull2) now the master branch should just say 'This is the first commit' "
